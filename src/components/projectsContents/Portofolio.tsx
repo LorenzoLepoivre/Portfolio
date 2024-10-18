9c21e203e05db6a774b0dc7b61693c864dfd8267
SupPepper <lorenzo.lepoivre@gmail.com>
2024-10-18 12:29:37 +0200

    Update Portofolio.tsx

--- src/components/projectsContents/Portofolio.tsx
+++ src/components/projectsContents/Portofolio.tsx
@@ -2,6 +2,7 @@ function Portofolio() {
     return (
       <div>
         <p>This website you are currently viewing showcases my major projects as well as my resume.</p>
+        <p>It's made with react.</p>
       </div>
     );
   }

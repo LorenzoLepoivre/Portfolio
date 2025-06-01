03466ec275793de221ec1aea4402c59db0440429
SupPepper <lorenzo.lepoivre@gmail.com>
2025-06-01 09:47:57 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,13 @@
 import Header from '../components/Header';
 import Project from '../components/Project';
 import Tourniquet from '../components/Tourniquet';
-import Portofolio from '../components/projectsContents/Portofolio';
 import MintFiesta from '../components/projectsContents/MintFiesta';
 import R6DLE from '../components/projectsContents/R6dle';
 import DarkAndLight from '../components/projectsContents/DarkAndLight';
 import SuiBox from '../components/projectsContents/SuiBox';
-import '../css/home.css'; // Importer le fichier CSS avec le bon chemin
+import '../css/home.css';
 
 function Home() {
-  let imagesPortofolio = ["img/projects/portofolio/1.png"];
   let imagesMintFiesta = ["img/projects/mintFiesta/1.jpg","img/projects/mintFiesta/2.jpg","img/projects/mintFiesta/3.jpg","img/projects/mintFiesta/4.jpg","img/projects/mintFiesta/5.jpg","img/projects/mintFiesta/6.jpg"];
   let r6dleVid = ["https://youtu.be/GPMdjfOXNEc"];
   let darkAndLightVid = ["https://youtu.be/T2Estex6_PI"];
